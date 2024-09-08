682fd22e93d4fee0f50b64ec810f4d30392f50f5
NguyenTrien <trienntph41852@fpt.edu.vn>
2024-09-09 00:53:57 +0700

    test1

--- app/src/main/java/com/thuydev/saydream/Activity/ActivityBegin.java
+++ app/src/main/java/com/thuydev/saydream/Activity/ActivityBegin.java
@@ -24,6 +24,7 @@ public class ActivityBegin extends AppCompatActivity {
                     public void Callback() {
                         finish();
                     }
+
                     
                 });
             }
